a9812108db6b7e95b22c08d9492f4a12d97ecdc6
ardu1na <arduinadelbosque@gmail.com>
2023-06-14 14:20:34 -0300

    commented send_request view

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
-    path('test/<int:id>/', views.test, name="test")
+    path('send/<int:id>/', views.send_request, name="send") # send point request
+    
 ]
--- app/views.py
+++ app/views.py
@@ -6,15 +6,32 @@ from django.contrib.auth.decorators import login_required
 from app.models import Worker, Punto, Registro
 
 
-@login_required
-def test(request, id):
+# ####################
+# LOGICA PARA EL REGISTRO DE LOS SERENOS
+# PASO X PUNTOS DE CONTROL
+#
+
+# requiere estar loggeado, sino redirecciona al login
+@login_required 
+def send_request(request, id):
+    # solicita el id del punto de control
+    # EL ID va como parámetro en el qr
+    
+    # identifica el trabajador en base al usuario loggeado
     worker_id = request.user.id
     worker = get_object_or_404(Worker, id=worker_id)
     print(worker)
+    
+    # identifica al punto de control 
     punto = get_object_or_404(Punto, id=id)
     print(punto)
+    
+    # crea el registro
+    # para el empleado, en ese momento, en ese punto de control
     new_registro = Registro.objects.create(worker=worker, punto=punto)
     print(new_registro)
-
-
+    
+    # respuesta al usuario
     return HttpResponse(f"Solicitud de {worker} en {punto} \n {new_registro}")
+#
+# ####################
